6c847dbeae14be40fc1fabf9fb5337245edbe824
Dist <vlad244244@gmail.com>
2024-10-04 10:54:12 +0300

    _

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -32,6 +32,11 @@ GLfloat lightPos[4] = {0.0f, 0.0f, -10.0f, 1.0f}; // Начальное поло
 
 GLuint texture;
 
+// Параметры материала
+GLfloat matDiffuse[] = {0.8f, 0.2f, 0.2f, 1.0f}; // Рассеянный свет
+GLfloat matSpecular[] = {1.0f, 1.0f, 1.0f, 1.0f}; // Спекулярный свет
+GLfloat matShininess[] = {50.0f}; // Яркость
+
 void createCheckerboardTexture() {
     // Генерация шахматной текстуры (черно-белая)
     for (int i = 0; i < TEXTURE_HEIGHT; i++) {
@@ -57,9 +62,13 @@ void createCheckerboardTexture() {
 }
 
 void update_camera_position() {
-    camera_pos[0] = 2 * cos(t) + 2*sin(t);
+    camera_pos[0] = 2 * cos(t) + 2 * sin(t);
     camera_pos[1] = 2 * sin(t);
     camera_pos[2] = -10.0f;
+    
+    // Анимация источника света
+    lightPos[0] = 2 * cos(t) + 2 * sin(t);
+    lightPos[1] = 2 * sin(t);
     t += 0.01f;
     if (t > 2 * M_PI) {
         t -= 2 * M_PI;
@@ -83,6 +92,10 @@ void init() {
     glLightfv(GL_LIGHT0, GL_SPECULAR, lightColor);
     glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
 
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, matDiffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, matSpecular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, matShininess);
+
     glEnable(GL_COLOR_MATERIAL);
     glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
 
@@ -94,29 +107,67 @@ void init() {
     createCheckerboardTexture();
 }
 
+// Вычисление нормали на основе векторов
+void computeNormal(GLfloat* v1, GLfloat* v2, GLfloat* v3, GLfloat* normal) {
+    GLfloat u[3], v[3];
+
+    // Вектор u = v2 - v1
+    u[0] = v2[0] - v1[0];
+    u[1] = v2[1] - v1[1];
+    u[2] = v2[2] - v1[2];
+
+    // Вектор v = v3 - v1
+    v[0] = v3[0] - v1[0];
+    v[1] = v3[1] - v1[1];
+    v[2] = v3[2] - v1[2];
+
+    // Векторное произведение u и v для получения нормали
+    normal[0] = u[1] * v[2] - u[2] * v[1];
+    normal[1] = u[2] * v[0] - u[0] * v[2];
+    normal[2] = u[0] * v[1] - u[1] * v[0];
+
+    // Нормализация нормали
+    GLfloat length = sqrt(normal[0] * normal[0] + normal[1] * normal[1] + normal[2] * normal[2]);
+    if (length != 0) {
+        normal[0] /= length;
+        normal[1] /= length;
+        normal[2] /= length;
+    }
+}
+
 void drawPrism() {
-    // Рисуем нижнюю трапецию с текстурными координатами
+    GLfloat normal[3];
+
+    // Рисуем нижнюю трапецию с текстурными координатами и нормалью
+    computeNormal(baseVertices[0], baseVertices[1], baseVertices[2], normal);
     glBindTexture(GL_TEXTURE_2D, texture);
     glBegin(GL_QUADS);
+    glNormal3fv(normal); // Нормаль для нижней грани
     for (int i = 0; i < 4; i++) {
         glTexCoord2fv(textureCoords[i]);  // Текстурные координаты
         glVertex3fv(baseVertices[i]);     // Нижняя трапеция
     }
     glEnd();
 
-    // Рисуем верхнюю трапецию с текстурными координатами
+    // Рисуем верхнюю трапецию с текстурными координатами и нормалью
+    computeNormal(topVertices[0], topVertices[1], topVertices[2], normal);
     glBegin(GL_QUADS);
+    glNormal3fv(normal); // Нормаль для верхней грани
     for (int i = 0; i < 4; i++) {
         glTexCoord2fv(textureCoords[i]);  // Те же текстурные координаты
         glVertex3fv(topVertices[i]);      // Верхняя трапеция
     }
     glEnd();
 
-// Рисуем боковые грани призмы с новыми текстурными координатами
+    // Рисуем боковые грани призмы с нормалями
     glBegin(GL_QUADS);
     for (int i = 0; i < 4; i++) {
         int next = (i + 1) % 4;
-
+        
+        // Вычисляем нормаль для боковой грани
+        computeNormal(baseVertices[i], baseVertices[next], topVertices[next], normal);
+        glNormal3fv(normal); // Устанавливаем нормаль для боковой грани
+        
         // Задаем текстурные координаты для боковых граней
         glTexCoord2f(0.0f, 0.0f);
         glVertex3fv(baseVertices[i]);
